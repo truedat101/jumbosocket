ab93bc0ab8aaa3aa5e73bea72c73afe1b0abdebb
David J. Kordsmeier <devnull@localhost>
2011-04-18 12:57:12 -0700

    Added document.ready onto the script.  Wow, this was bad.  Need to make sure the document is loaded and ready before firing up the socket.  Otherwise, we may actually load the script, start execution, and not have had time to load socket.io.js yet, and then get and UndefinedReference.   And this may or may not happen consistently.  So better to be safe.

--- js/script.js
+++ js/script.js
@@ -1,27 +1,29 @@
-var socket = new io.Socket('localhost', {
-}); 
-var connectcount = 0;
-var disconnectcount = 0;
-var messagecount = 0;
-var sentmessagecount = 0;
+$(document).ready(function() {
+	var socket = new io.Socket('localhost', {
+	}); 
+	var connectcount = 0;
+	var disconnectcount = 0;
+	var messagecount = 0;
+	var sentmessagecount = 0;
 
-socket.connect();
-socket.on('connect', function(){
-	connectcount = connectcount + 1;
-	$('#connectstats').text(connectcount);
-});
-socket.on('disconnect', function(){
-	disconnectcount = disconnectcount + 1;
-	$('#disconnectstats').text(disconnectcount);
-});
-socket.on('message', function(){
-	messagecount = messagecount + 1;
-	$('#messagestats').text(messagecount);
-});
+	socket.connect();
+	socket.on('connect', function(){
+		connectcount = connectcount + 1;
+		$('#connectstats').text(connectcount);
+	});
+	socket.on('disconnect', function(){
+		disconnectcount = disconnectcount + 1;
+		$('#disconnectstats').text(disconnectcount);
+	});
+	socket.on('message', function(){
+		messagecount = messagecount + 1;
+		$('#messagestats').text(messagecount);
+	});
 
-$('#send').click(function() {
-	socket.send('Send a generic message');
-	sentmessagecount = sentmessagecount + 1;
-	$('#sentmessagestats').text(sentmessagecount);
-	return false;
+	$('#send').click(function() {
+		socket.send('Send a generic message');
+		sentmessagecount = sentmessagecount + 1;
+		$('#sentmessagestats').text(sentmessagecount);
+		return false;
+	});
 });
