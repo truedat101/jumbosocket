a05a12f56191775822998591c9881525a8358220
David J. Kordsmeier <dkords@gmail.com>
2011-08-16 09:11:56 -0700

    Fix for message encoding.  Information on this (yet another) api change that fails silently is here: https://github.com/LearnBoost/Socket.IO/wiki/Migrating-0.6-to-0.7

--- js/script.js
+++ js/script.js
@@ -22,7 +22,7 @@ $(document).ready(function() {
 
 	$('#send').click(function() {
 		// socket.send("{msg: 'session@default' , op: 'handshake', payload: 'hello'}");
-		socket.send({'msg': 'session@default' , 'op': 'handshake', 'payload': 'hello'});
+		socket.json.send({'msg': 'session@default' , 'op': 'handshake', 'payload': 'hello'});
 		sentmessagecount = sentmessagecount + 1;
 		$('#sentmessagestats').text(sentmessagecount);
 		return false;
