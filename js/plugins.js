24ee09f8bd8bfd740b442a3144bd7f3bb7f991ba
David J. Kordsmeier <dkords@gmail.com>
2011-08-21 01:43:38 -0700

    Added latest html5boilerplate.  Refactored script naming.  New library is calls js-common.js to contain all basic jumbosocket calls.  Moved libraries into /js/libs.  Upped version.

--- js/plugins.js
+++ js/plugins.js
@@ -1,40 +1,11 @@
 
-// remap jQuery to $
-(function($){
-
- 
-
-
-
-
-
- 
-
-
-
-})(window.jQuery);
-
-
-
-// usage: log('inside coolFunc',this,arguments);
-// paulirish.com/2009/log-a-lightweight-wrapper-for-consolelog/
+// usage: log('inside coolFunc', this, arguments);
 window.log = function(){
   log.history = log.history || [];   // store logs to an array for reference
   log.history.push(arguments);
-  if(this.console){
-    console.log( Array.prototype.slice.call(arguments) );
+  if(this.console) {
+      arguments.callee = arguments.callee.caller;
+      console.log( Array.prototype.slice.call(arguments) );
   }
 };
-
-
-
-// catch all document.write() calls
-(function(doc){
-  var write = doc.write;
-  doc.write = function(q){ 
-    log('document.write(): ',arguments); 
-    if (/docwriteregexwhitelist/.test(q)) write.apply(doc,arguments);  
-  };
-})(document);
-
-
+(function(b){function c(){}for(var d="assert,count,debug,dir,dirxml,error,exception,group,groupCollapsed,groupEnd,info,log,markTimeline,profile,profileEnd,time,timeEnd,trace,warn".split(","),a;a=d.pop();)b[a]=b[a]||c})(window.console=window.console||{});
